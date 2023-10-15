2a2f5f54d14ee0fa06a2f532b0af58c0dc7cb4a2
Ashray Jha <jha.ashray12@gmail.com>
2023-10-15 13:26:09 +0530

    Time: 2 ms (47.49%), Space: 6.2 MB (52.74%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,28 +1,51 @@
 class Solution {
-    //memoization 
+   
+    
+//     //memoization 
 
-    private:
-         int DistinctWays(int n , vector<int>& dp){
-             if(n<=1){
-                 return 1;
-             }
-             if(dp[n]!= -1){
-                 return dp[n];
-             }
+//     private:
+//          int DistinctWays(int n , vector<int>& dp){
+//              if(n<=1){
+//                  return 1;
+//              }
+//              if(dp[n]!= -1){
+//                  return dp[n];
+//              }
              
-          return dp[n] = DistinctWays(n-1,dp)+DistinctWays(n-2,dp);
+//           return dp[n] = DistinctWays(n-1,dp)+DistinctWays(n-2,dp);
              
-             
+//          }
+    
+    
+    
+// public:
+//     int climbStairs(int n) {
+//         vector<int>dp(n+1,-1);
+//         return DistinctWays(n,dp);
+        
+        
+        
+//     }
+    
+    
+    public : 
+    //Space Optimized, qns similar to Fibonnaci
+    
+         int climbStairs(int n){
+  
+        int prev2= 1;
+        int prev = 1;
+        int curr_i;
+        for(int i =2 ;i<=n;i++){
+            curr_i = prev2 + prev;
+            prev2 = prev;
+            prev = curr_i;    
+        }
+        
+       return prev;
+    
+    
+    
          }
     
-    
-    
-public:
-    int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return DistinctWays(n,dp);
-        
-        
-        
-    }
 };
